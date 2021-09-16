75b9814445eca8df08c9e3b2e3bf89821dba1d4e
Alyona <Guinerva@yandex.ru>
2021-09-17 01:44:12 +0300

    Доверстала Locations,выполнила дз по js.

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,15 @@
-console.log('script work!');
+$(document).ready(function() {
+
+    $('.team-link').on('click', function(event) {
+        event.preventDefault();
+
+        $('.team-link').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('.location-link').on('click', function(event) {
+        event.preventDefault();
+
+    });
+
+});
