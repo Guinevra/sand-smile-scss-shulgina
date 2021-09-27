84e6698e05c17bb3b36ee6d8a4fd8fc3348cac17
Alyona <Guinerva@yandex.ru>
2021-09-27 23:45:30 +0300

    Добавила действия по наведению мышки в скциях Location и в header

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -2,20 +2,36 @@ $(document).ready(function() {
 
     //Team
 
-    $('.team-link').on('click', function(event) {
-        event.preventDefault();
+    $('.team-link').on('click', function(e) {
+        e.preventDefault();
+
 
         $('.team-link').removeClass('active');
         $(this).addClass('active');
+
+
+        let index = $(this).index('.team-link');
+
+        $('.team-desc').removeClass('active');
+        $('.team-desc').eq(index).addClass('active');
+
     });
 
-
-
-
-    //Слайдер
-
+    //Gallery
     $('.j-slider').slick({
         dots: true
     });
+
+
+    //Location
+    $('.location-link').on('click', function(e) {
+        e.preventDefault();
+
+
+    });
+
+
+
+
 
 });
